291c23c52079af47e8bf5502dce16177c8c12460
Gabriel Strobel <gabrielcoelho91@gmail.com>
2023-11-22 14:44:45 -0300

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Fira_Code } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 const firaCode = Fira_Code({ subsets: ['latin'] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={firaCode.className}>{children}</body>
+      <body className={firaCode.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
